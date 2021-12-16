19686e56aa70451c65707e98101c460f6e9ab370
Julie <jq.nikitina@gmail.com>
2021-12-16 23:27:31 +0700

    Connect to mongo

--- app.js
+++ app.js
@@ -3,19 +3,25 @@ const mongoose = require("mongoose");
 const app = express();
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb ', {
+mongoose.connect('mongodb://localhost:27017/mydb', {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+  // useCreateIndex: true,
+  // useFindAndModify: false
+}, err => {
+  if (err) {
+    console.error('Unable to connect to mongodb', err);
+  } else {
+    console.log('Successfully connected to mongodb');
+  }
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
-  };
-
-  next();
-});
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+//   };
+//
+//   next();
+// });
 
 
 app.listen(PORT, () => {
