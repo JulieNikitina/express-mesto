ff7a382dff9744cc0ff1111c8d62e0138261f703
Julie <jq.nikitina@gmail.com>
2021-12-30 20:30:45 +0700

    Хеширование пароля, дефолтные значения для полей

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,6 @@
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 const BAD_REQUEST_CODE = 400;
@@ -6,74 +9,79 @@ const INTERNAL_SERVER_ERROR_CODE = 500;
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ users }))
-    .catch(() => res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла ошибка' }));
+    .then((users) => res.send({users}))
+    .catch(() => res.status(INTERNAL_SERVER_ERROR_CODE).send({message: 'Произошла ошибка'}));
 };
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (user) {
-        res.send({ user });
+        res.send({user});
       } else {
-        res.status(NOT_FOUND_CODE).send({ message: 'Пользователь не найден' });
+        res.status(NOT_FOUND_CODE).send({message: 'Пользователь не найден'});
       }
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_CODE).send({ message: 'Переданы некорректные данные' });
+        res.status(BAD_REQUEST_CODE).send({message: 'Переданы некорректные данные'});
       } else {
-        res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла ошибка' });
+        res.status(INTERNAL_SERVER_ERROR_CODE).send({message: 'Произошла ошибка'});
       }
     });
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => res.status(201).send({ user }))
+  const {email, password, name, about, avatar} = req.body;
+  if (validator.isEmail(email) !== true) {
+    res.status(BAD_REQUEST_CODE).send({message: 'То что вы ввели - не имеил'});
+    return
+  }
+  bcrypt.hash(password, 10)
+    .then(hash => User.create({email, password: hash, name, about, avatar})
+      .then((user) => res.status(201).send({user})))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(BAD_REQUEST_CODE).send({ message: 'Переданы некорректные данные' });
+        res.status(BAD_REQUEST_CODE).send({message: 'Переданы некорректные данные'});
       } else {
-        res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла ошибка' });
+        res.status(INTERNAL_SERVER_ERROR_CODE).send({message: 'Произошла ошибка'});
       }
     });
 };
 
 module.exports.updateUserInfo = (req, res) => {
-  const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+  const {name, about} = req.body;
+  User.findByIdAndUpdate(req.user._id, {name, about}, {new: true, runValidators: true})
     .then((user) => {
       if (user) {
-        res.send({ user });
+        res.send({user});
       } else {
-        res.status(NOT_FOUND_CODE).send({ message: 'Пользователь не найден' });
+        res.status(NOT_FOUND_CODE).send({message: 'Пользователь не найден'});
       }
     })
     .catch((err) => {
       if (err.name === 'ValidationError' || err.name === 'CastError') {
-        res.status(BAD_REQUEST_CODE).send({ message: 'Переданы некорректные данные' });
+        res.status(BAD_REQUEST_CODE).send({message: 'Переданы некорректные данные'});
       } else {
-        res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла ошибка' });
+        res.status(INTERNAL_SERVER_ERROR_CODE).send({message: 'Произошла ошибка'});
       }
     });
 };
 
 module.exports.updateAvatar = (req, res) => {
-  const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+  const {avatar} = req.body;
+  User.findByIdAndUpdate(req.user._id, {avatar}, {new: true, runValidators: true})
     .then((user) => {
       if (user) {
-        res.send({ user });
+        res.send({user});
       } else {
-        res.status(NOT_FOUND_CODE).send({ message: 'Пользователь не найден' });
+        res.status(NOT_FOUND_CODE).send({message: 'Пользователь не найден'});
       }
     })
     .catch((err) => {
       if (err.name === 'ValidationError' || err.name === 'CastError') {
-        res.status(BAD_REQUEST_CODE).send({ message: 'Переданы некорректные данные' });
+        res.status(BAD_REQUEST_CODE).send({message: 'Переданы некорректные данные'});
       } else {
-        res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Произошла ошибка' });
+        res.status(INTERNAL_SERVER_ERROR_CODE).send({message: 'Произошла ошибка'});
       }
     });
 };
